ebbe45fbd29193f54cde726b90f10e253ffc1b7e
Rodrigo Gonzalez <rgonzalez@telelistas.net>
2013-12-20 20:16:29 -0200

    Add projeto museu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Museum::Application.config.secret_token = '74b46b34034445be9c4e9037a5ae4642b6d69b66bb27d4a9da5cc668a8dfcb6d6722638122682464f4487d143789aa269cb4bc4cba11495178797d4324442dc9'
